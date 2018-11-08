f32f7d412078581d4937a48df44178459b8e1e58
Steven Wong <stevenwongyt@gmail.com>
2018-11-08 17:28:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 var kittens = [] //define your array here
 
 // Add your functions and code here
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function appendKitten(name) {
+  return [...kittens, name];
+}
+
+function prependKitten(name) {
+  return [name, ...kittens];
+}
+
+function removeLastKitten() {
+  kittens.slice(0, kittens.length - 1);
+}
+
+function removeFirstKitten () {
+  kittens.slice(0,0);
+}
